ce2a42be051588230e48e26bacc6f4074b629df4
vipullsayal <117041143+vipullsayal@users.noreply.github.com>
2026-02-28 12:23:46 +0000

    Update app.py

--- consumer/app.py
+++ consumer/app.py
@@ -5,18 +5,18 @@ from confluent_kafka import Consumer, KafkaError, KafkaException, Producer
 # Create consumer
 c = Consumer(
     {
-        'bootstrap.servers': '<ccloud bootstrap servers>',
+        'bootstrap.servers': 'vskafkalab1.servicebus.windows.net:9093',
         'sasl.mechanism': 'PLAIN',
         'security.protocol': 'SASL_SSL',
-        'sasl.username': '<ccloud key>',
-        'sasl.password': '<ccloud secret>',
+        'sasl.username': '$ConnectionString',
+        'sasl.password': 'Endpoint=sb://vskafkalab1.servicebus.windows.net/;SharedAccessKeyName=RootManageSharedAccessKey;SharedAccessKey=iId8K7LaPMa5fKxpJ3GB8SBVMp01IjfmR+AEhBfbtdA=;EntityPath=vspyhontopic',
         'group.id': str(uuid.uuid1()),  # this will create a new consumer group on each invocation.
         'auto.offset.reset': 'earliest' #,
         # 'error_cb': error_cb,
     }
 )
 
-c.subscribe(['python-test-topic'])
+c.subscribe(['vspyhontopic'])
 
 try:
     while True:
